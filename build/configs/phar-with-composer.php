0b5408ad04cae097f129c25153401ca49ef2cb39
Tom Flídr <tomflidr@gmail.com>
2019-01-04 17:28:25 +0100

    Pig-Latin translator portable skeleton structure

---
+++ build/configs/phar-with-composer.php
@@ -0,0 +1,45 @@
+<?php
+
+$config = [
+	'sourcesDir'			=> __DIR__ . '/../../development',
+	'releaseFile'			=> __DIR__ . '/../../release/index.php',
+	// do not include script or file, where it's relative path from sourceDir match any of these rules:
+	'excludePatterns'		=> [
+		
+		// Common excludes for every MvcCore app using composer:
+		"#/\.#",										// Everything started with '.' (.git, .htaccess ...)
+		"#^/web\.config#",								// Microsoft IIS .rewrite rules
+		"#^/Var/Logs/.*#",								// App development logs
+		"#(composer|installed)\.((dev\.)?)(json|lock)#",// composer.json, installed.json, composer.lock, ...
+		"#LICEN(C|S)E(\.(txt|md))?#i",					// libraries licence files
+		"#\.(bak|bat|cmd|sh|md|phpt|phpproj|phpproj\.user)$#i",
+
+		// Exclude specific PHP libraries
+		"#^/vendor/tracy/.*#",							// - tracy library (https://tracy.nette.org/)
+		"#^/vendor/tracy/tracy/(.*)/assets/#",			//   excluded everything except statically
+		"#^/vendor/tracy/tracy/tools/#",				//   loaded PHP scripts by composer - added later
+		"#^/vendor/mvccore/ext-debug-tracy.*#",			// - mvccore tracy adapter and all tracy panel extensions
+		"#^/vendor/mrclay/.*#",							// - HTML/JS/CSS minify library
+		
+		// Exclude source css and js files, use only what is generated in '/Var/Tmp' dir
+		"#^/static/js#",
+		"#^/static/css#",
+	],
+	// include all scripts or files, where it's relative path from sourceDir match any of these rules:
+	// (include paterns always overrides exclude patterns)
+	'includePatterns'		=> [
+		// add statically included tracy file back again and override it's exclusion,
+		// to run composer at application start properly, but this file will not be used.
+		"#^/vendor/tracy/tracy/src/shortcuts.php#",
+	],
+	// process simple strings replacements on all read PHP scripts before saving into result package:
+	// (replacements are executed before configured minification in RAM, they don't affect anything on hard drive)
+	'stringReplacements'	=> [
+		// Switch \MvcCore application back from SFU mode to automatic compile mode detection
+		'->Run(1);'									=> '->Run();',
+		// Remove tracy debug library:
+		"class_exists('\MvcCore\Ext\Debugs\Tracy')"	=> 'FALSE',
+	],
+	'minifyTemplates'		=> 1,// Remove non-conditional comments and white spaces
+	'minifyPhp'				=> 1,// Remove comments and white spaces
+];
---
+++ build/configs/php-with-composer-strict-package.php
@@ -0,0 +1,46 @@
+<?php
+
+$phpFileSystemMode = Packager_Php::FS_MODE_STRICT_PACKAGE;
+
+$config = [
+	'sourcesDir'				=> __DIR__ . '/../../development',
+	'releaseFile'				=> __DIR__ . '/../../release/index.php',
+	// do not include script or file, where it's relative path from sourceDir match any of these rules:
+	'excludePatterns'			=> [
+		
+		// Common excludes for every \MvcCore app using composer:
+		"#/\.#",										// Everything started with '.' (.git, .htaccess ...)
+		"#^/web\.config#",								// Microsoft IIS .rewrite rules
+		"#^/Var/Logs/.*#",								// App development logs
+		"#(composer|installed)\.((dev\.)?)(json|lock)#",// composer.json, installed.json, composer.lock, ...
+		"#LICEN(C|S)E(\.(txt|md))?#i",					// libraries licence files
+		"#\.(bak|bat|cmd|sh|md|phpt|phpproj|phpproj\.user)$#i",
+		
+		// Exclude specific PHP libraries
+		"#^/vendor/composer/.*#",						// composer itself
+		"#^/vendor/autoload\.php$#",					// composer autoload file
+		"#^/vendor/mvccore/mvccore/src/startup\.php$#",	// mvccore autoload file
+		"#^/vendor/tracy/.*#",							// tracy library (https://tracy.nette.org/)
+		"#^/vendor/mvccore/ext-debug-tracy.*#",			// mvccore tracy adapter and all tracy panel extensions
+		"#^/vendor/mrclay/.*#",							// HTML/JS/CSS minify library
+
+		// Exclude source css and js files, use only what is generated in '/Var/Tmp' dir
+		"#^/static/js#",
+		"#^/static/css#",
+	],
+	// include all scripts or files, where it's relative path from sourceDir match any of these rules:
+	// (include patterns always overrides exclude patterns)
+	'includePatterns'		=> [],
+	// process simple strings replacements on all read PHP scripts before saving into result package:
+	// (replacements are executed before configured minification in RAM, they don't affect anything on hard drive)
+	'stringReplacements'	=> [
+		// Switch \MvcCore application back from SFU mode to automatic compile mode detection
+		'->Run(1);'									=> '->Run();',
+		'->Run(TRUE);'								=> '->Run();',
+		'->Run(true);'								=> '->Run();',
+		// Remove tracy debug library:
+		"class_exists('\MvcCore\Ext\Debugs\Tracy')"	=> 'FALSE',
+	],
+	'minifyTemplates'		=> 1,// Remove non-conditional comments and white spaces
+	'minifyPhp'				=> 1,// Remove comments and white spaces
+];
